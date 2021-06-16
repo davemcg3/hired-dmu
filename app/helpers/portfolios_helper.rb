4a6d3ddceb3b83546a1b70d46669356f79b20f3c
davemcg3 <dave.mcgrath3@gmail.com>
2021-06-15 20:35:25 -0700

    Fixing runtime errors

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -4,7 +4,7 @@ module PortfoliosHelper
   end
 
   def AddYours(user)
-    return (button_to 'Set yours to public!', edit_portfolio_path(user.portfolio), method: :get).html_safe if user.portfolio
+    return (button_to 'Set yours to public!', edit_portfolio_path(user.portfolio), method: :get).html_safe if user.present? and user.portfolio
     (button_to 'Add Yours!', new_portfolio_path, method: :get)
   end
 end
